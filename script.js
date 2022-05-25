5a0f45211b1891070fe2efa82c07000ac1491023
Пользователь <petrsolomonov4@gmail.com>
2022-05-25 21:01:10 +0900

    homework 1

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "");
-console.log(numberOfFilms);
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "");
+const personalMovieDB = {
+    privat: false,
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
