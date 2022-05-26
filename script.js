17335f6747242d8c5bc60cff4742b19e60a89664
Пользователь <petrsolomonov4@gmail.com>
2022-05-26 21:30:17 +0900

    add new cycle

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ const personalMovieDB = {
     actors: {},
     genres: [],
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+
+
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+
+    if (a != null && b != null && a != '' && b != '' && String.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Сделано')
+    } else {
+        i--;
+    }
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
